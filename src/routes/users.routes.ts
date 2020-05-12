edab7757743d1338b4470d119d0cae2eb481b070
joao nogueira <joaomenna1@gmail.com>
2020-05-12 00:23:23 -0400

    :arrow_up: melhorando exeption errors e add routes avatar

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,28 +1,48 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
+
+import ensureAuthenticate from '../middleware/ensureAthenticante';
 
 const userRouters = Router();
+const upload = multer(uploadConfig);
 
 userRouters.post('/', async (req, res) => {
-  try {
-    const { name, email, password } = req.body;
+  const { name, email, password } = req.body;
 
-    const createUser = new CreateUserService();
+  const createUser = new CreateUserService();
 
-    const user = await createUser.execute({
-      name,
-      email,
-      password,
+  const user = await createUser.execute({
+    name,
+    email,
+    password,
+  });
+
+  delete user.password;
+
+  return res.json(user);
+});
+
+userRouters.patch(
+  '/avatar',
+  ensureAuthenticate,
+  upload.single('avatar'),
+  async (req, res) => {
+    const updateUserAvatar = new UpdateUserAvatarService();
+
+    const user = await updateUserAvatar.execute({
+      user_id: req.user.id,
+      AvatarFilename: req.file.filename,
     });
 
     delete user.password;
 
     return res.json(user);
-  } catch (err) {
-    return res.status(400).json({ error: err.message });
   }
-});
+);
 
 export default userRouters;
 
