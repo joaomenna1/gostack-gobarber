bccdbc7acd6068001bde49ce4b56a1413524c982
joao nogueira <joaomenna1@gmail.com>
2020-07-26 02:53:53 -0400

    :rocket: userTokensRepository

--- src/modules/users/repositories/IUserTokensRepository.ts
+++ src/modules/users/repositories/IUserTokensRepository.ts
@@ -2,5 +2,5 @@ import UserToken from '../infra/typeorm/entities/UserTokens';
 
 export default interface IUserTokensRepository {
   generate(user_id: string): Promise<UserToken>;
-  findByToken(token: string): Promise<UserToken | underfine>;
+  findByToken(token: string): Promise<UserToken | undefined>;
 }
