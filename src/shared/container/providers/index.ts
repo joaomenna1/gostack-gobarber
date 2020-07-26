da0a4909e796b7a1f079d00ac8fe32b6f797d2a8
joao nogueira <joaomenna1@gmail.com>
2020-07-26 02:50:41 -0400

    add injections dependeces

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -3,7 +3,23 @@ import { container } from 'tsyringe';
 import IStorageProvider from './StorageProvider/models/IStorageProvider';
 import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
 
+import IMailProvider from './MailProvider/model/IMailProvider';
+import EtherealMailProvider from './MailProvider/implementations/EtheralEmailProvider';
+
+import IMailTemplateProvider from './MailTemplateProvider/model/IMailTemplateProvider';
+import HandleBarsMailTemplateProvider from './MailTemplateProvider/implementations/HandleBarsMailTemplateProvider';
+
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
   DiskStorageProvider
+);
+
+container.registerInstance<IMailProvider>(
+  'MailProvider',
+  new EtherealMailProvider()
+);
+
+container.registerSingleton<IMailTemplateProvider>(
+  'MailTemplateProvider',
+  HandleBarsMailTemplateProvider
 );
