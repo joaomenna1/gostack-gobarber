0483d66b1582db698d7bc9b38bb979ad5ba7d3de
joao nogueira <joaomenna1@gmail.com>
2020-07-26 02:53:10 -0400

    :bug: fix name inject

--- src/modules/users/services/AuthenticateUserService.ts
+++ src/modules/users/services/AuthenticateUserService.ts
@@ -20,7 +20,7 @@ interface IResponse {
 @injectable()
 class AuthenticateUserService {
   constructor(
-    @inject('UserRepository')
+    @inject('UsersRepository')
     private usersRepository: IUsersRepository,
     @inject('HashProvider')
     private hashProvider: IHashProvider
