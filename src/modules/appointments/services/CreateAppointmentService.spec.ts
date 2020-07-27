142d13424cd75b75a9fdae4c91de5c46225ac630
joao nogueira <joaomenna1@gmail.com>
2020-07-27 00:42:30 -0400

    refeactoring tests

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -2,11 +2,16 @@ import AppError from '@shared/errors/AppError';
 import FakeAppointmentsRepository from '../repositories/fakes/fakeAppointmentsRepository';
 import CreateAppointment from './CreateAppointmentService';
 
-describe('CreateAppointment', () => {
-  it('should be able to create a new appointment', async () => {
-    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
-    const createAppointment = new CreateAppointment(fakeAppointmentsRepository);
+let fakeAppointmentsRepository: FakeAppointmentsRepository;
+let createAppointment: CreateAppointment;
 
+describe('CreateAppointment', () => {
+  beforeEach(() => {
+    fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    createAppointment = new CreateAppointment(fakeAppointmentsRepository);
+  });
+
+  it('should be able to create a new appointment', async () => {
     const appointment = await createAppointment.execute({
       date: new Date(),
       provider_id: '123132',
@@ -17,9 +22,6 @@ describe('CreateAppointment', () => {
   });
 
   it('should not be able to create two appointments on the same time', async () => {
-    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
-    const createAppointment = new CreateAppointment(fakeAppointmentsRepository);
-
     const appointmentDate = new Date(2020, 4, 10, 11);
 
     await createAppointment.execute({
--- src/modules/users/services/test/AuthenticateUserService.spec.ts
+++ src/modules/users/services/test/AuthenticateUserService.spec.ts
@@ -4,21 +4,25 @@ import AuthenticateUserService from '../AuthenticateUserService';
 import CreateUserServices from '../CreateUserService';
 import FakeHashProvider from '../../providers/hashProvider/fakes/FakeHashProvider';
 
+let fakeUsersRepository: FakeUsersRepository;
+let fakeHashProvider: FakeHashProvider;
+let createUser: CreateUserServices;
+let authenticateUser: AuthenticateUserService;
+
 describe('AuthenticateUserService', () => {
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+    fakeHashProvider = new FakeHashProvider();
+
+    authenticateUser = new AuthenticateUserService(
+      fakeUsersRepository,
+      fakeHashProvider
+    );
+
+    createUser = new CreateUserServices(fakeUsersRepository, fakeHashProvider);
+  });
+
   it('should be able to authenticate', async () => {
-    const fakeUserRepository = new FakeUsersRepository();
-    const fakeHashProvider = new FakeHashProvider();
-
-    const authenticateUser = new AuthenticateUserService(
-      fakeUserRepository,
-      fakeHashProvider
-    );
-
-    const createUser = new CreateUserServices(
-      fakeUserRepository,
-      fakeHashProvider
-    );
-
     const user = await createUser.execute({
       name: 'John Doe',
       email: 'john@gmail.com',
@@ -35,15 +39,7 @@ describe('AuthenticateUserService', () => {
   });
 
   it('should not be able to authenticate with non existing user', async () => {
-    const fakeUserRepository = new FakeUsersRepository();
-    const fakeHashProvider = new FakeHashProvider();
-
-    const authenticateUser = new AuthenticateUserService(
-      fakeUserRepository,
-      fakeHashProvider
-    );
-
-    expect(
+    await expect(
       authenticateUser.execute({
         email: 'john@gmail.com',
         password: '123456',
@@ -53,26 +49,13 @@ describe('AuthenticateUserService', () => {
 });
 
 it('should not be able to authenticate with wrong password', async () => {
-  const fakeUserRepository = new FakeUsersRepository();
-  const fakeHashProvider = new FakeHashProvider();
-
-  const authenticateUser = new AuthenticateUserService(
-    fakeUserRepository,
-    fakeHashProvider
-  );
-
-  const createUser = new CreateUserServices(
-    fakeUserRepository,
-    fakeHashProvider
-  );
-
   await createUser.execute({
     name: 'John Doe',
     email: 'john@gmail.com',
     password: '123456',
   });
 
-  expect(
+  await expect(
     authenticateUser.execute({
       email: 'john@gmail.com',
       password: 'wrong-password',
