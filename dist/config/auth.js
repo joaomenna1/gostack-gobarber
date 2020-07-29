a9da4f637fe81a7c5f7ebf7628082a407ec30361
joao nogueira <joaomenna1@gmail.com>
2020-07-29 01:39:22 -0400

    :rocket: test appointments/ show profile/forgot/listProviders

---
+++ dist/config/auth.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    expiresIn: '1d',
+    secret_Authenticate: '8538607221f2e42284acf599214cfa34',
+};
